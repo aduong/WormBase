5b6a9f364261cbf4e90aec1e84347faa010fdc27
Adrian Duong <adrian.duong@oicr.on.ca>
2011-11-22 18:52:48 -0500

    Log to a logfile

--- t/WormBase/API/Benchmark/bypass_model/concurrent.pl
+++ t/WormBase/API/Benchmark/bypass_model/concurrent.pl
@@ -8,6 +8,8 @@ use Ace::Couch;
 use Class::MOP;
 use Getopt::Long;
 use Parallel::ForkManager;
+use Time::HiRes qw(time);;
+use Fcntl qw(:flock);
 
 use constant HOST          => 'dev.wormbase.org';
 use constant PORT          => 2005;
@@ -15,15 +17,19 @@ use constant TEST_SIZE     => 20_000;
 use constant WORK_PER_FORK => 100;
 
 my ($TYPE, $WORKERS);
+my $LOGFILE = "$$.log";
 GetOptions(
     'type=s'    => \$TYPE,
     'workers=i' => \$WORKERS,
+    'logfile=s' => \$LOGFILE,
 );
 
 die "--type invalid\n"
     unless defined $TYPE && Class::MOP::is_class_loaded($TYPE);
 die "--workers must be non-negative integer\n"
     unless defined $WORKERS && $WORKERS =~ /^[0-9]+$/;
+
+open my $logfh, '>', $LOGFILE;
 
 my $pm = Parallel::ForkManager->new($WORKERS);
 
@@ -43,15 +49,27 @@ while () {
         },
     ) or die 'Could not connect: ', $TYPE->error;
 
-    my $obj;
+    print 'Startup ', $$, "\n";
+
+    my $total_time = 0;
+    my ($obj, $t1, $t2);
     foreach ($start..$end) {
         $obj = sprintf('WBGene%08i', $_);
-        $dbh->fetch(
+        $t1 = time;
+        $obj = $dbh->fetch(
             -class => 'Gene',
             -name  => $obj,
             -fill  => 1,
-        ) and print $obj, "\n";
+        );
+        $t2 = time;
+        $total_time += $t2 - $t1 if $obj;
     }
+
+    if (flock $logfh, LOCK_EX) {
+        print {$logfh} $$, '; time: ', $total_time, " s\n";
+    }
+
+    print 'Shutdown ', $$, "\n";
 
     $pm->finish;
 }
